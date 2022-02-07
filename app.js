624bf1005b07304412b42e2300c7ca4227b92239
apham1594 <88216187+apham1594@users.noreply.github.com>
2022-02-07 03:17:43 -0700

    made changes

---
+++ app.js
@@ -0,0 +1,24 @@
+var button = document.querySelector('.button')
+var inputValue= document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+
+button.addEventListener('click', function(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=ab2265222413cb3e1744360bd07c7ad6`)
+    .then(response => response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var temp = data['main']['temp'];
+        var descValue = data['weather'][0]['description'];
+
+        name.innnerHTML = nameValue;
+        temp.innnerHTML=tempValue;
+        desc.innerHTML=descValue;
+    })
+
+.catch(err => alert("wrong city name!"))
+})
+
+
